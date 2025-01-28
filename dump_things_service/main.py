847e7284a191b6b93a0b52692bd612353256e2aa
Christian Monch <christian.moench@web.de>
2025-01-28 10:11:29 +0100

    add application and version handling

--- dump_things_service/main.py
+++ dump_things_service/main.py
@@ -1,31 +1,15 @@
+from collections import defaultdict
+from itertools import count
 from typing import Annotated
 
 from fastapi import FastAPI, Form
-from pydantic import BaseModel
-from itertools import count
 
 from model import build_model, get_classes
 
 
-
-# Create pydantic models
-
-model = build_model('https://concepts.trr379.de/s/base/unreleased.yaml')
-classes = get_classes(model)
-print(classes)
-
-
-app = FastAPI()
-
-
-
-class FormData(BaseModel):
-    name: str
-    email: str
-    zusage: str
-    food: str
-    infos: str
-    model_config = {"extra": "forbid"}
+schema_sources = {
+    'trr379': ['https://concepts.trr379.de/s/base/unreleased.yaml'],
+}
 
 
 _endpoint_template = """
@@ -34,23 +18,45 @@ def {name}(data: Annotated[model.{type}, Form(), {info}]):
     return data
 """
 
+
+models = defaultdict(dict)
+
+# Create pydantic models
+for schema_name, schema_urls in schema_sources.items():
+    for schema_url in schema_urls:
+        model = build_model(schema_url)
+        classes = get_classes(model)
+        models[schema_name][model.version] = model, classes
+
+
+app = FastAPI()
+
+
+# Create endpoints for all applications, all versions, and all classes
 serial_number = count()
+for application, app_info in models.items():
+    for version, (model, classes) in app_info.items():
+        for type_name in classes:
 
+            # Create an endpoint to dump data of type `type_name` in version
+            # `version` of schema `application`.
+            endpoint_name = f'_endpoint_{next(serial_number)}'
+            exec(_endpoint_template.format(
+                name=endpoint_name,
+                type=type_name,
+                info=f"'endpoint for {application}/{version}/{type_name}'"
+            ))
+            endpoint = locals()[endpoint_name]
 
-for type_name in classes:
-    endpoint_name = f'_endpoint_{next(serial_number)}'
-    exec(_endpoint_template.format(
-        name=endpoint_name,
-        type=type_name,
-        info=f"'endpoint {endpoint_name} to dump {type_name}'"
-    ))
-    app.add_api_route(
-        path=f'/dump/{type_name}',
-        endpoint=locals()[endpoint_name],
-        methods=['POST'],
-        name=f'Added endpoint "{endpoint_name}" to receive "{type_name}" objects',
-        response_model=getattr(model, type_name)
-    )
+            # Create an API route for the endpoint
+            app.add_api_route(
+                path=f'/dump/{application}/{version}/{type_name}',
+                endpoint=locals()[endpoint_name],
+                methods=['POST'],
+                name=f'Added "{endpoint_name}" to handle "{application}/{version}/{type_name}" objects',
+                response_model=getattr(model, type_name)
+            )
+
 
 app.openapi_schema = None
 app.setup()
