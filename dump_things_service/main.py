df230ca9f5124c14cd4eed4fa876a76c6ed471ec
Christian Monch <christian.moench@web.de>
2025-01-29 20:20:49 +0100

    use `id` mainly

--- dump_things_service/main.py
+++ dump_things_service/main.py
@@ -28,12 +28,12 @@ lgr = logging.getLogger('uvicorn')
 
 
 # Create pydantic models from schema sources
-models = defaultdict(dict)
+models = {}
 for schema_source in schema_sources:
     lgr.info('Building model from %s.', schema_source)
     model = build_model(schema_source)
     classes = get_classes(model)
-    models[model.linkml_meta['name']][model.version] = model, classes
+    models[schema_source] = model.linkml_meta['name'], model.version, model, classes
 
 
 app = FastAPI()
@@ -42,28 +42,29 @@ app = FastAPI()
 # Create endpoints for all applications, all versions, and all classes
 lgr.info('Creating dynamic endpoints...')
 serial_number = count()
-for application, app_info in models.items():
-    for version, (model, classes) in app_info.items():
-        for type_name in classes:
-            # Create an endpoint to dump data of type `type_name` in version
-            # `version` of schema `application`.
-            endpoint_name = f'_endpoint_{next(serial_number)}'
-            exec(_endpoint_template.format(
-                name=endpoint_name,
-                type=type_name,
-                id=model.linkml_meta['id'],
-                info=f"'endpoint for {application}/{version}/{type_name}'"
-            ))
-            endpoint = locals()[endpoint_name]
 
-            # Create an API route for the endpoint
-            app.add_api_route(
-                path=f'/dump/{application}/{version}/{type_name}',
-                endpoint=locals()[endpoint_name],
-                methods=['POST'],
-                name=f'handle "{type_name}" of schema "{model.linkml_meta["id"]}" objects',
-                response_model=getattr(model, type_name)
-            )
+#for application, app_info in models.items():
+for model_id, (name, version, model, classes) in models.items():
+    for type_name in classes:
+        # Create an endpoint to dump data of type `type_name` in version
+        # `version` of schema `application`.
+        endpoint_name = f'_endpoint_{next(serial_number)}'
+        exec(_endpoint_template.format(
+            name=endpoint_name,
+            type=type_name,
+            id=model.linkml_meta['id'],
+            info=f"'endpoint for {application}/{version}/{type_name}'"
+        ))
+        endpoint = locals()[endpoint_name]
+
+        # Create an API route for the endpoint
+        app.add_api_route(
+            path=f'/dump/{application}/{version}/{type_name}',
+            endpoint=locals()[endpoint_name],
+            methods=['POST'],
+            name=f'handle "{type_name}" of schema "{model.linkml_meta["id"]}" objects',
+            response_model=getattr(model, type_name)
+        )
 
 lgr.info('Creation of %d endpoints completed.', next(serial_number))
 
