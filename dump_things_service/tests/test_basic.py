b81771c3e05d631337b1dc196802115df5755990
Christian Monch <christian.moench@web.de>
2025-02-03 15:01:06 +0100

    rename test source

--- dump_things_service/tests/test_basic.py
+++ dump_things_service/tests/test_basic.py
@@ -1,52 +1,34 @@
 import json
+import sys
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from unittest.mock import patch
 
 from fastapi.testclient import TestClient
 
-from .create_store import create_store
+from .create_store import create_stores
 
 
 temp_dir_obj = TemporaryDirectory()
 temp_dir = Path(temp_dir_obj.name)
 
 
-create_store(
-    temp_dir / 'global_store',
-    {
+create_stores(
+    root_dir=temp_dir,
+    collection_info={
         'schema_1': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5'),
         'schema_2': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5-p3'),
         'schema_3': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1'),
         'schema_4': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1-p3'),
         'schema_5': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'after-last-colon'),
-    }
-)
-
-create_store(
-    temp_dir / 'token1_store',
-    {
-        'schema_1': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5'),
-        'schema_2': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5-p3'),
-        'schema_3': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1'),
-        'schema_4': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1-p3'),
-        'schema_5': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'after-last-colon'),
-    }
+    },
+    token_stores=['token_1'],
 )
 
 
-test_config = f"""
-global_store: {str(temp_dir / 'global_store')}
-token_stores:
-  token1: {str(temp_dir / 'token1_store')}
-"""
-
-(temp_dir / 'test_config.yaml').write_text(test_config)
-
-
-with patch.dict('os.environ', {'DUMPTHINGS_CONFIG_FILE': str(temp_dir / 'test_config.yaml')}):
-    from ..main import app
-
+old_sys_argv = sys.argv
+sys.argv = ['test', str(temp_dir)]
+from ..main import app
+sys.argv = old_sys_argv
 
 client = TestClient(app)
 
@@ -61,7 +43,7 @@ def test_store_record():
     for i in range(1, 6):
         response = client.post(
             f'/schema_{i}/record/InstantaneousEvent',
-            headers={'x-dumpthings-token': 'token1'},
+            headers={'x-dumpthings-token': 'token_1'},
             data={'id': 'aaaa'}
         )
         assert response.status_code == 200
