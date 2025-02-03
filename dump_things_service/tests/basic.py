a4967f63a435202ad8aabeb673f11b688e54d015
Christian Monch <christian.moench@web.de>
2025-02-03 09:05:14 +0100

    adapt tests to modified URLs

--- dump_things_service/tests/basic.py
+++ dump_things_service/tests/basic.py
@@ -51,7 +51,30 @@ with patch.dict('os.environ', {'DUMPTHINGS_CONFIG_FILE': str(temp_dir / 'test_co
 client = TestClient(app)
 
 
-def test_find():
+def test_search_by_id():
     for i in range(1, 6):
-        response = client.get(f'/schema_{i}/find/1111')
+        response = client.get(f'/schema_{i}/record/1111')
         assert json.loads(response.text) == {'type': 'dltemporal:InstantaneousEvent', 'id': '1111'}
+
+
+def test_store_record():
+    for i in range(1, 6):
+        response = client.post(
+            f'/schema_{i}/record/InstantaneousEvent',
+            headers={'x-dumpthings-token': 'token1'},
+            data={'id': 'aaaa'}
+        )
+        assert response.status_code == 200
+
+    for i in range(1, 6):
+        response = client.get(f'/schema_{i}/record/aaaa')
+        assert response.status_code == 200
+
+
+def test_global_store_fails():
+    for i in range(1, 6):
+        response = client.post(
+            f'/schema_{i}/record/InstantaneousEvent',
+            data={'id': 'aaaa'}
+        )
+        assert response.status_code == 422
