6ea18e36191c54da9aca4559019c309ab364a414
Christian Monch <christian.moench@web.de>
2025-02-03 06:39:36 +0100

    add a simple test data store creator

--- dump_things_service/tests/basic.py
+++ dump_things_service/tests/basic.py
@@ -1,27 +1,57 @@
+import json
 from pathlib import Path
 from tempfile import TemporaryDirectory
 from unittest.mock import patch
 
 from fastapi.testclient import TestClient
 
+from .create_store import create_store
+
 
 temp_dir_obj = TemporaryDirectory()
 temp_dir = Path(temp_dir_obj.name)
-(temp_dir / 'config.yaml').write_text("""
-global_store: global
-token_stores:
-    token1: token1
-    token2: token2
-"""
+
+
+create_store(
+    temp_dir / 'global_store',
+    {
+        'schema_1': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5'),
+        'schema_2': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5-p3'),
+        'schema_3': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1'),
+        'schema_4': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1-p3'),
+        'schema_5': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'after-last-colon'),
+    }
+)
+
+create_store(
+    temp_dir / 'token1_store',
+    {
+        'schema_1': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5'),
+        'schema_2': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-md5-p3'),
+        'schema_3': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1'),
+        'schema_4': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'digest-sha1-p3'),
+        'schema_5': ('https://concepts.trr379.de/s/base/unreleased.yaml', 'after-last-colon'),
+    }
 )
 
 
-with patch.dict('os.environ', {'DUMPTHINGS_CONFIG_FILE': str(temp_dir / 'config.yaml')}):
+test_config = f"""
+global_store: {str(temp_dir / 'global_store')}
+token_stores:
+  token1: {str(temp_dir / 'token1_store')}
+"""
+
+(temp_dir / 'test_config.yaml').write_text(test_config)
+
+
+with patch.dict('os.environ', {'DUMPTHINGS_CONFIG_FILE': str(temp_dir / 'test_config.yaml')}):
     from ..main import app
 
 
 client = TestClient(app)
 
 
-def test_docs():
-    pass
+def test_find():
+    for i in range(1, 6):
+        response = client.get(f'/schema_{i}/find/1111')
+        assert json.loads(response.text) == {'type': 'dltemporal:InstantaneousEvent', 'id': '1111'}
