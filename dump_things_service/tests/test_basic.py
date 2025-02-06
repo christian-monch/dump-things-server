02aeafdbdce9182794bf048ff7fa31317136a718
Christian Monch <christian.moench@web.de>
2025-02-06 10:49:30 +0100

    add a minimal schema and minimal test fixtures

--- dump_things_service/tests/test_basic.py
+++ dump_things_service/tests/test_basic.py
@@ -3,48 +3,75 @@ from urllib.parse import quote
 
 from .create_store import (
     identifier,
-    dump_stores,
-    fastapi_app,
-    fastapi_client,
+    dump_stores_simple,
+    fastapi_app_simple,
+    fastapi_client_simple,
+    given_name,
 )
 
 
-def test_search_by_id(fastapi_client):
-    test_client, _ = fastapi_client
+extra_record = {'id': 'aaaa', 'given_name': 'David'}
+
+
+def test_search_by_id(fastapi_client_simple):
+    test_client, _ = fastapi_client_simple
     for i in range(1, 6):
-        response = test_client.get(f'/schema_{i}/record?id={quote(identifier)}')
-        assert json.loads(response.text) == {'type': 'dltemporal:InstantaneousEvent', 'id': identifier}
+        response = test_client.get(f'/store_{i}/record?id={quote(identifier)}')
+        assert json.loads(response.text) == {'id': identifier, 'given_name': given_name}
 
 
-def test_store_record(fastapi_client):
-    test_client, _ = fastapi_client
+def test_store_record(fastapi_client_simple):
+    test_client, _ = fastapi_client_simple
     for i in range(1, 6):
         response = test_client.post(
-            f'/schema_{i}/record/InstantaneousEvent',
+            f'/store_{i}/record/Person',
             headers={'x-dumpthings-token': 'token_1'},
-            json={'id': 'aaaa'}
+            json=extra_record
         )
         assert response.status_code == 200
 
+    # Check that all stores can retrieve the record
     for i in range(1, 6):
-        response = test_client.get(f'/schema_{i}/record?id=aaaa')
+        response = test_client.get(
+            f'/store_{i}/record?id={extra_record["id"]}',
+            headers={'x-dumpthings-token': 'token_1'},
+        )
         assert response.status_code == 200
+        assert json.loads(response.text) == extra_record
+
+    # Check that all stores report two records, i.e. the default record
+    # and the newly added record
+    for i in range(1, 6):
+        response = test_client.get(
+            f'/store_{i}/records/Person',
+            headers={'x-dumpthings-token': 'token_1'},
+       )
+        assert sorted(
+            json.loads(response.text),
+            key=lambda x: x['id'],
+        ) == sorted(
+            [
+                {'id': identifier, 'given_name': given_name},
+                {'id': 'aaaa', 'given_name': 'David'},
+            ],
+            key=lambda x: x['id'],
+        )
 
 
-def test_global_store_fails(fastapi_client):
-    test_client, _ = fastapi_client
+def test_global_store_fails(fastapi_client_simple):
+    test_client, _ = fastapi_client_simple
     for i in range(1, 6):
         response = test_client.post(
-            f'/schema_{i}/record/InstantaneousEvent',
+            f'/store_{i}/record/Person',
             json={'id': 'aaaa'}
         )
         assert response.status_code == 422
 
 
-def test_token_store_adding(fastapi_client):
-    test_client, store_dir = fastapi_client
+def test_token_store_adding(fastapi_client_simple):
+    test_client, store_dir = fastapi_client_simple
     response = test_client.post(
-        '/schema_1/record/InstantaneousEvent',
+        '/store_1/record/Person',
         headers={'x-dumpthings-token': 'david_bowie'},
         json={'id': 'aaaa'}
     )
@@ -53,19 +80,19 @@ def test_token_store_adding(fastapi_client):
     # Create token directory and retry
     (store_dir / 'token_stores' / 'david_bowie').mkdir()
     response = test_client.post(
-        '/schema_1/record/InstantaneousEvent',
+        '/store_1/record/Person',
         headers={'x-dumpthings-token': 'david_bowie'},
         json={'id': 'aaaa'}
     )
     assert response.status_code == 200
 
 
-def test_funky_id(fastapi_client):
-    test_client, _ = fastapi_client
-    record_id = 'trr379:contributors/stupid'
+def test_funky_id(fastapi_client_simple):
+    test_client, _ = fastapi_client_simple
+    record_id = 'trr379:contributors/someone'
     for i in range(1, 6):
         response = test_client.post(
-            f'/schema_{i}/record/InstantaneousEvent',
+            f'/store_{i}/record/Person',
             headers={'x-dumpthings-token': 'token_1'},
             json={'id': record_id}
         )
@@ -74,7 +101,7 @@ def test_funky_id(fastapi_client):
     # Try to find it
     for i in range(1, 6):
         response = test_client.get(
-            f'/schema_{i}/record?id={record_id}',
+            f'/store_{i}/record?id={record_id}',
             headers={'x-dumpthings-token': 'token_1'},
         )
         assert response.status_code == 200
