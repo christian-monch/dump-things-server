d397fc508064cd75852415f57d3b549ee81edd6c
Christian Monch <christian.moench@web.de>
2025-02-12 15:36:09 +0100

    add regression test for identifier mappings

---
+++ dump_things_service/tests/test_mapping.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+import yaml
+
+from . import HTTP_200_OK
+
+identifier = 'abc:this_is_an_identifier'
+
+record_a = {
+    'id': identifier,
+    'given_name': 'James',
+}
+
+record_b = {
+    'id': identifier,
+    'given_name': 'Zulu',
+}
+
+
+def test_mapping_functions_ignore_data(fastapi_client_simple):
+    test_client, store_path = fastapi_client_simple
+
+    for i in range(1, 6):
+        response = test_client.post(
+            f'/store_{i}/record/Person',
+            headers={'x-dumpthings-token': 'token_1'},
+            json=record_a,
+        )
+        assert response.status_code == HTTP_200_OK
+
+        response = test_client.post(
+            f'/store_{i}/record/Person',
+            headers={'x-dumpthings-token': 'token_1'},
+            json=record_b,
+        )
+        assert response.status_code == HTTP_200_OK
+
+        # Check that only one record with the given id exists in the store
+        id_counter = defaultdict(int)
+        token_store_path = store_path / 'token_stores' / 'token_1' / f'store_{i}'
+        for path in token_store_path.rglob('*.yaml'):
+            if path.is_file():
+                record = yaml.load(path.read_text(), Loader=yaml.SafeLoader)
+                id_counter[record['id']] += 1
+        assert all(v == 1 for v in id_counter.values())
