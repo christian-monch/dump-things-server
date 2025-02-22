734267585f2358d5c226adf3144d4cc709187b34
Christian Monch <christian.moench@web.de>
2025-02-23 00:34:59 +0100

    simplify pydantic model creation

--- dump_things_service/model.py
+++ dump_things_service/model.py
@@ -1,23 +1,11 @@
 from __future__ import annotations
 
-import dataclasses  # noqa F401 -- used by generated code
-import importlib
-import subprocess
-import tempfile
 import types
 from itertools import count
-from pathlib import Path
 from typing import Any, TYPE_CHECKING
 
-import annotated_types  # noqa F401 -- used by generated code
-import pydantic  # noqa F401 -- used by generated code
-import pydantic_core  # noqa F401 -- used by generated code
+from linkml.generators import PydanticGenerator
 from pydantic._internal._model_construction import ModelMetaclass
-
-from dump_things_service.utils import (
-    read_url,
-    sys_path,
-)
 
 if TYPE_CHECKING:
     import types
@@ -28,18 +16,7 @@ serial_number = count()
 def build_model(
     source_url: str,
 ) -> types.ModuleType:
-    with tempfile.TemporaryDirectory() as temp_dir:
-        module_name = f'model_{next(serial_number)}'
-        definition_file = Path(temp_dir) / 'definition.yaml'
-        definition_file.write_text(read_url(source_url))
-        subprocess.run(
-            args=['gen-pydantic', str(definition_file)],
-            stdout=(Path(temp_dir) / (module_name + '.py')).open('w'),
-            check=True,
-        )
-        with sys_path([temp_dir]):
-            model = importlib.import_module(module_name)
-    return model
+    return PydanticGenerator(source_url).compile_module()
 
 
 def get_classes(
