d6e17f82bc4638c101feb037f04e05acd3176588
Christian Monch <christian.moench@web.de>
2025-06-26 14:52:37 +0200

    add arguments for record dir store and config path

--- dump_things_service/graphql/main.py
+++ dump_things_service/graphql/main.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
+import argparse
+import logging
 import sys
-from enum import Enum
 from pathlib import Path
 from typing import (
     cast,
     Any,
-    Iterable,
     Optional,
 )
 
@@ -27,10 +27,49 @@ from dump_things_service.model import get_model_for_schema
 from dump_things_service.record import RecordDirStore
 
 
-# The next steps generate the GraphQL-module. This is needed for the resolver-
-# signatures.
-record_dir_root = Path(sys.argv[1])
-config = Config.get_collection_dir_config(record_dir_root)
+logger = logging.getLogger('dump_things_service.graphql')
+uvicorn_logger = logging.getLogger('uvicorn')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--host', default='0.0.0.0')  # noqa S104
+parser.add_argument('--port', default=8000, type=int)
+parser.add_argument('--origins', action='append', default=['*'])
+parser.add_argument(
+    '--root-path',
+    default='',
+    help="Set the ASGI 'root_path' for applications submounted below a given URL path.",
+)
+parser.add_argument(
+    '--log-level',
+    default='WARNING',
+    help="Set the log level for the service, allowed values are 'ERROR', 'WARNING', 'INFO', 'DEBUG'. Default is 'warning'.",
+)
+parser.add_argument(
+    'config_dir',
+    help='Directory that contains the config file for store.',
+)
+parser.add_argument(
+    'store',
+    help='The directory that stores the records.',
+)
+
+arguments = parser.parse_args()
+
+# Set the log level
+numeric_level = getattr(logging, arguments.log_level.upper(), None)
+if not isinstance(numeric_level, int):
+    logger.error('Invalid log level: %s, defaulting to level "WARNING"', arguments.log_level)
+    numeric_level = getattr(logging, 'WARNING', None)
+
+logging.basicConfig(level=numeric_level)
+
+
+# The next steps generate the GraphQL module. This is needed for the
+# resolver-signatures.
+config_dir = Path(arguments.config_dir)
+record_root_dir = Path(arguments.store)
+config = Config.get_collection_dir_config(config_dir)
 graphql_module = get_strawberry_module_for_linkml_schema(config.schema)
 linkml_model, classes, model_var_name = get_model_for_schema(config.schema)
 
@@ -40,7 +79,7 @@ ClassNames = graphql_module.ClassNames
 
 
 store = RecordDirStore(
-    root=record_dir_root,
+    root=record_root_dir,
     model=linkml_model,
     pid_mapping_function=get_mapping_function(config),
     suffix=config.format,
@@ -62,28 +101,27 @@ def resolve_records(class_name: ClassNames) -> list[AllThings]:
     return list(get_records_by_class_name(graphql_module, store, class_name.value))
 
 
-class LogLevel(str, Enum):
-    debug = "debug"
-    info = "info"
-    warning = "warning"
-    error = "error"
-
-
-def server(
-        record_dir_root: Path,
-        host: str = '0.0.0.0',
-        port: int = 8000,
-        log_level: LogLevel = LogLevel.debug,
-) -> None:
-
+def server() -> None:
+    host = arguments.host
+    port = arguments.port
 
     # Windows doesn't support UTF-8 by default
     endl = " 🍓\n" if sys.platform != "win32" else "\n"
-    print(f"Running strawberry on http://{host}:{port}/graphql, using store: {config.schema}", end=endl)  # noqa: T201
+    print(
+        f'Running strawberry on http://{host}:{port}/graphql,\n'
+        f'  store: {record_root_dir}\n'
+        f'  config dir: {config_dir}\n'
+        f'  schema: {config.schema}\n',
+        end=endl
+    )
 
     app = Starlette(debug=True)
     app.add_middleware(
-        CORSMiddleware, allow_headers=["*"], allow_origins=["*"], allow_methods=["*"]
+        CORSMiddleware,
+        allow_origins=arguments.origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
     )
 
     @strawberry.type
@@ -104,9 +142,9 @@ def server(
         app,
         host=host,
         port=port,
-        log_level=log_level,
+        log_level=numeric_level,
     )
 
 
 if __name__ == "__main__":
-    server(record_dir_root)
+    server()
