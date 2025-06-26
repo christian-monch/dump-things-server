c221673444df56c00fdd6372ac30f85ea6190402
Christian Monch <christian.moench@web.de>
2025-06-26 11:55:13 +0200

    convert schema-relations in graphql-relations
    This commit adds code to convert relations in a
    record (dictionaries) into the GraphQL-schema
    relations (lists of identifiers).

--- dump_things_service/graphql/resolvers.py
+++ dump_things_service/graphql/resolvers.py
@@ -8,7 +8,6 @@ from typing import (
 
 import strawberry
 
-from dump_things_service import JSON
 from dump_things_service.model import get_subclasses
 from dump_things_service.record import RecordDirStore
 from dump_things_service.resolve_curie import resolve_curie
@@ -40,8 +39,8 @@ def get_record_by_pid(
     iri = resolve_curie(dir_store.model, pid)
     class_name, record = dir_store.get_record_by_iri(iri)
     if class_name and record:
-        inlined_record = _inline_relations(strawberry_module, dir_store, record)
-        return getattr(strawberry_module, class_name)(**inlined_record)
+        _convert_relations(record)
+        return getattr(strawberry_module, class_name)(**record)
     return None
 
 
@@ -56,34 +55,12 @@ def get_records_by_class_name(
     for subclass_name in get_subclasses(dir_store.model, class_name):
         for _, record in dir_store.get_records_of_class(subclass_name):
             if record:
+                _convert_relations(record)
                 yield getattr(strawberry_module, subclass_name)(**record)
 
 
-def _inline_relations(
-        strawberry_module: types.ModuleType,
-        dir_store: RecordDirStore,
-        record: dict,
-) -> Any:
-    if not record.get('relations'):
-        return record
-
-    new_relations = []
-    for pid in record['relations']:
-        related_class_name, related_record = dir_store.get_record_by_iri(
-            resolve_curie(dir_store.model, pid)
-        )
-        if not related_class_name or not related_record:
-            msg = f"Related record not found for PID: {pid}"
-            raise ValueError(msg)
-
-        new_relations.append(
-            getattr(strawberry_module, related_class_name)(
-                **_inline_relations(
-                    strawberry_module,
-                    dir_store,
-                    related_record
-                )
-            )
-        )
-    record['relations'] = new_relations
-    return record
+def _convert_relations(
+    record: dict,
+) -> None:
+    if record.get('relations'):
+        record['relations'] = list(record['relations'])
