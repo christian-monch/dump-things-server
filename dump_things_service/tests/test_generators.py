b453e97f04f09791a656714b433a61ff9ccc768d
Christian Monch <christian.moench@web.de>
2025-10-08 14:51:35 +0200

    support fully merged schemas
    Add a script that generates a fully merged schema.
    This script generates a simpler output than
    `gen-yaml`. It does not track the original sources
    of definitions.
    
    The patch to `linkml_runtime.utils.enumerations`
    became necessary with more complex schemata,
    especially with merged schemas.

---
+++ dump_things_service/tests/test_generators.py
@@ -0,0 +1,20 @@
+# The tests below require patches
+import dump_things_service.patches.enabled
+
+from pathlib import Path
+
+from dump_things_service.model import (
+    get_model_for_schema,
+    get_schema_model_for_schema,
+)
+
+# Path to a local simple test schema
+schema_dir = Path(__file__).parent / 'assets'
+
+
+def test_gen_pydantic():
+    get_model_for_schema(str(schema_dir / 'schema-merged.yaml'))
+
+
+def test_gen_python():
+    get_schema_model_for_schema(str(schema_dir / 'schema-merged.yaml'))
