9e37a6d890842ecc2ed1bc57e18ee13b23810533
Christian Monch <christian.moench@web.de>
2025-11-06 21:11:56 +0100

    update any_of_test

--- any_of_test/dataclass_test.py
+++ any_of_test/dataclass_test.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
-from typing import Union
-#from schema_python import Thing
-from dataclasses import dataclass, asdict as dataclass_asdict
-from dataclass_wizard import asdict, fromdict, LoadMeta
+from schema_python_new import Thing
 
 from linkml_runtime.utils.schemaview import SchemaView
+from dacite import from_dict, Config
 
 
-#sv = SchemaView('schema.yaml')
+sv = SchemaView('schema.yaml')
+
+
 
 
 json_obj_1 = {
@@ -20,65 +20,35 @@ json_obj_2 = {
     "pid": "thing_2",
     "multislot": [
         {
-            "__tag__": "ClassA",
             "pid": "pid_a_1",
             "name_a": "name_a_1"
         },
         {
-            "__tag__": "ClassA",
             "pid": "pid_a_2",
             "name_a": "name_a_2"
         },
         {
-            "__tag__": "ClassB",
             "pid": "pid_b_1",
             "name_b": "name_b_1"
         }
     ]
 }
 
-@dataclass
-class ClassA:
-    pid: str
-    name_a: str
-
-
-@dataclass
-class ClassB:
-    pid: str
-    name_b: str
-
-@dataclass
-class Thing:
-    pid: str
-    multislot: list[Union[ClassA, ClassB]]
-
-
-x = fromdict(Thing, json_obj_1)
-print(x)
-
-
-x = Thing(**json_obj_1)
-print(x)
-
-#x = Thing(**json_obj_2)
-#print(x)
-#print(dataclass_asdict(x))
-
-
-x = Thing(
-    pid="thing_1",
-    multislot=[
-        ClassA(pid="a_1", name_a="a_1"),
-        ClassB(pid="b_1", name_b="b_1"),
-    ]
+from linkml.utils.datautils import (
+    get_dumper,
+    get_loader,
 )
-print(x)
-print(dataclass_asdict(x))
 
+loader = get_loader('json')
 
-y = asdict(x)
-print(y)
+data_obj = from_dict(
+    Thing,
+    json_obj_2,
+    Config(strict=True, strict_unions_match=True),
+)
 
-z = fromdict(Thing, json_obj_2)
-print(z)
+print(data_obj)
+
+dumper = get_dumper('ttl')
+ttl_object = dumper.dumps(data_obj, schemaview=sv)
+print(ttl_object)
---
+++ any_of_test/schema_python_new.py
@@ -0,0 +1,186 @@
+# Manually edited from:
+# Auto generated from schema.yaml by pythongen.py version: 0.0.1
+# Generation date: 2025-11-06T21:03:59
+# Schema: any_of_test_schema
+#
+# id: http://example.org/any-of-test-schema
+# description:
+# license: https://creativecommons.org/publicdomain/zero/1.0/
+
+from __future__ import annotations
+
+import dataclasses
+import re
+from dataclasses import dataclass
+from datetime import (
+    date,
+    datetime,
+    time
+)
+from typing import (
+    Any,
+    ClassVar,
+    Dict,
+    List,
+    Optional,
+    Union
+)
+
+from jsonasobj2 import (
+    JsonObj,
+    as_dict
+)
+from linkml_runtime.linkml_model.meta import (
+    EnumDefinition,
+    PermissibleValue,
+    PvFormulaOptions
+)
+from linkml_runtime.utils.curienamespace import CurieNamespace
+from linkml_runtime.utils.enumerations import EnumDefinitionImpl
+from linkml_runtime.utils.formatutils import (
+    camelcase,
+    sfx,
+    underscore
+)
+from linkml_runtime.utils.metamodelcore import (
+    bnode,
+    empty_dict,
+    empty_list
+)
+from linkml_runtime.utils.slot import Slot
+from linkml_runtime.utils.yamlutils import (
+    YAMLRoot,
+    extended_float,
+    extended_int,
+    extended_str
+)
+from rdflib import (
+    Namespace,
+    URIRef
+)
+
+from linkml_runtime.linkml_model.types import String
+
+metamodel_version = "1.7.0"
+version = None
+
+# Namespaces
+ABC = CurieNamespace('abc', 'http://example.org/person-schema/abc/')
+LINKML = CurieNamespace('linkml', 'https://w3id.org/linkml/')
+DEFAULT_ = ABC
+
+
+# Types
+
+# Class references
+
+
+
+Any = Any
+
+@dataclass(repr=False)
+class Thing(YAMLRoot):
+    _inherited_slots: ClassVar[list[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = ABC["Thing"]
+    class_class_curie: ClassVar[str] = "abc:Thing"
+    class_name: ClassVar[str] = "Thing"
+    class_model_uri: ClassVar[URIRef] = ABC.Thing
+
+    pid: Optional[str] = None
+    multislot: Optional[list[Union[ClassA, ClassB]]] = empty_list()
+
+    def __post_init__(self, *_: str, **kwargs: Any):
+        if self.pid is not None and not isinstance(self.pid, str):
+            self.pid = str(self.pid)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass(repr=False)
+class ClassA(YAMLRoot):
+    _inherited_slots: ClassVar[list[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = ABC["ClassA"]
+    class_class_curie: ClassVar[str] = "abc:ClassA"
+    class_name: ClassVar[str] = "ClassA"
+    class_model_uri: ClassVar[URIRef] = ABC.ClassA
+
+    pid: Optional[str] = None
+    name_a: Optional[str] = None
+
+    def __post_init__(self, *_: str, **kwargs: Any):
+        if self.pid is not None and not isinstance(self.pid, str):
+            self.pid = str(self.pid)
+
+        if self.name_a is not None and not isinstance(self.name_a, str):
+            self.name_a = str(self.name_a)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass(repr=False)
+class ClassB(YAMLRoot):
+    _inherited_slots: ClassVar[list[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = ABC["ClassB"]
+    class_class_curie: ClassVar[str] = "abc:ClassB"
+    class_name: ClassVar[str] = "ClassB"
+    class_model_uri: ClassVar[URIRef] = ABC.ClassB
+
+    pid: Optional[str] = None
+    name_b: Optional[str] = None
+
+    def __post_init__(self, *_: str, **kwargs: Any):
+        if self.pid is not None and not isinstance(self.pid, str):
+            self.pid = str(self.pid)
+
+        if self.name_b is not None and not isinstance(self.name_b, str):
+            self.name_b = str(self.name_b)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass(repr=False)
+class ClassC(YAMLRoot):
+    _inherited_slots: ClassVar[list[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = ABC["ClassC"]
+    class_class_curie: ClassVar[str] = "abc:ClassC"
+    class_name: ClassVar[str] = "ClassC"
+    class_model_uri: ClassVar[URIRef] = ABC.ClassC
+
+    pid: Optional[str] = None
+    name_c: Optional[str] = None
+
+    def __post_init__(self, *_: str, **kwargs: Any):
+        if self.pid is not None and not isinstance(self.pid, str):
+            self.pid = str(self.pid)
+
+        if self.name_c is not None and not isinstance(self.name_c, str):
+            self.name_c = str(self.name_c)
+
+        super().__post_init__(**kwargs)
+
+
+# Enumerations
+
+
+# Slots
+class slots:
+    pass
+
+slots.pid = Slot(uri=ABC.pid, name="pid", curie=ABC.curie('pid'),
+                 model_uri=ABC.pid, domain=None, range=Optional[str])
+
+slots.name_a = Slot(uri=ABC.name_a, name="name_a", curie=ABC.curie('name_a'),
+                    model_uri=ABC.name_a, domain=None, range=Optional[str])
+
+slots.name_b = Slot(uri=ABC.name_b, name="name_b", curie=ABC.curie('name_b'),
+                    model_uri=ABC.name_b, domain=None, range=Optional[str])
+
+slots.name_c = Slot(uri=ABC.name_c, name="name_c", curie=ABC.curie('name_c'),
+                    model_uri=ABC.name_c, domain=None, range=Optional[str])
+
+slots.multislot = Slot(uri=ABC.multislot, name="multislot", curie=ABC.curie('multislot'),
+                       model_uri=ABC.multislot, domain=None, range=Optional[list[Union[ClassA, ClassB]]])
